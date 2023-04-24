cca53394af1e26384dcb7e5722cecd8ed9107f20
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-04-25 00:09:09 +0200

    add changes2

--- js/task-05.js
+++ js/task-05.js
@@ -4,17 +4,30 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
+// const refs = {
+//   inputEl: document.getElementById("name-input"),
+//   nameEl: document.getElementById("name-output"),
+// };
+
+// refs.inputEl.addEventListener("input", onInputChange);
+
+// function onInputChange() {
+//   refs.nameEl.textContent = refs.inputEl.value;
+
+//   if (refs.inputEl.value.trim() === "") {
+//     refs.nameEl.textContent = "Anonymous";
+//   }
+// }
+
 const refs = {
   inputEl: document.getElementById("name-input"),
   nameEl: document.getElementById("name-output"),
 };
 
-refs.inputEl.addEventListener("input", onInputChange);
+refs.inputEl.addEventListener("input", (e) => {
+  refs.nameEl.textContent = e.currentTarget.value;
 
-function onInputChange() {
-  refs.nameEl.textContent = refs.inputEl.value;
-
-  if (refs.inputEl.value === "") {
+  if (e.currentTarget.value.trim() === "") {
     refs.nameEl.textContent = "Anonymous";
   }
-}
+});
--- js/task-07.js
+++ js/task-07.js
@@ -10,8 +10,31 @@ const refs = {
   textEl: document.getElementById("text"),
 };
 
+const data = {
+  inputEl: 0,
+  textEl: 0,
+  calc() {
+    this.inputEl = this.textEl;
+  },
+};
+
+dataFill();
+
 refs.inputEl.addEventListener("input", onInputChange);
 
-function onInputChange(e) {
-  refs.textEl.style.fontSize = `${e.target.value}px`;
+function onInputChange({ target }) {
+  const { value } = target;
+
+  if (target === refs.inputEl) {
+    refs.textEl.style.fontSize = `${value}px`;
+  }
+
+  target.setAttribute("value", value);
+  dataFill();
+  // refs.textEl.style.fontSize = `${e.target.value}px`
+}
+
+function dataFill() {
+  data.inputEl = Number(refs.inputEl.value);
+  data.textEl = Number(refs.textEl.value);
 }
